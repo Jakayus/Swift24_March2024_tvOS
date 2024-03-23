d6355c6702e3b6b5427e671c54a19ce0404cdcb9
Joel Sereno <jakayus123@gmail.com>
2024-03-23 09:48:49 -0700

    Create new empty tvOS project

---
+++ Swift_Quiz_Game/Swift_Quiz_Game/Swift_Quiz_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Quiz_GameApp.swift
+//  Swift_Quiz_Game
+//
+//  Created by Joel Sereno on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Quiz_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
